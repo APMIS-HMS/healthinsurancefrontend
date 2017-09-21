355e1c3eca3199fe074a922cf481feb9cb49edcf
aiyk <aiyk.ekwe@gmail.com>
2017-09-21 16:35:07 +0100

    new platform

--- src/app/system-modules/platform-owner/new-platform/new-platform.component.ts
+++ src/app/system-modules/platform-owner/new-platform/new-platform.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
+
+import { HeaderEventEmitterService } from '../../../services/event-emitters/header-event-emitter.service';
+
 
 @Component({
   selector: 'app-new-platform',
@@ -7,10 +12,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class NewPlatformComponent implements OnInit {
 
-  constructor() { }
-
-  ngOnInit() {
-    console.log('New platform');
+  saveBtn: string = "SAVE &nbsp; <i class='fa fa-check' aria-hidden='true'></i>";
+  platformFormGroup: FormGroup;
+  
+    constructor(
+      private _fb: FormBuilder,
+      private _toastr: ToastsManager,
+      private _headerEventEmitter: HeaderEventEmitterService,
+    ) { }
+  
+    ngOnInit() {
+      this._headerEventEmitter.setRouteUrl('New Platform');
+      this._headerEventEmitter.setMinorRouteUrl('');
+  
+      this.platformFormGroup = this._fb.group({
+        platformName: ['', [<any>Validators.required]],
+        email:  ['', [<any>Validators.required, <any>Validators.pattern('^([a-z0-9_\.-]+)@([\da-z\.-]+)(com|org|CO.UK|co.uk|net|mil|edu|ng|COM|ORG|NET|MIL|EDU|NG)$')]],
+        website: ['', [<any>Validators.required]],
+        address: ['', [<any>Validators.required]],
+        bankAccName: ['', [<any>Validators.required]],
+        bankAccNumber: ['', [<any>Validators.required]],
+        bc_fname: ['', [<any>Validators.required]],
+        bc_lname: ['', [<any>Validators.required]],
+        bc_email:  ['', [<any>Validators.required, <any>Validators.pattern('^([a-z0-9_\.-]+)@([\da-z\.-]+)(com|org|CO.UK|co.uk|net|mil|edu|ng|COM|ORG|NET|MIL|EDU|NG)$')]],        
+        bc_phone: ['', [<any>Validators.required]],
+        bc_position: ['', [<any>Validators.required]],
+        it_fname: ['', [<any>Validators.required]],
+        it_lname: ['', [<any>Validators.required]],
+        it_position: ['', [<any>Validators.required]],
+        it_email:  ['', [<any>Validators.required, <any>Validators.pattern('^([a-z0-9_\.-]+)@([\da-z\.-]+)(com|org|CO.UK|co.uk|net|mil|edu|ng|COM|ORG|NET|MIL|EDU|NG)$')]],        
+        it_phone: ['', [<any>Validators.required]],
+        plan: ['', [<any>Validators.required]],
+        // plan2: ['', [<any>Validators.required]],
+        // plan3: ['', [<any>Validators.required]],
+        nhisNumber: ['', [<any>Validators.required]],
+        cinNumber: ['', [<any>Validators.required]],
+        registrationDate: ['', [<any>Validators.required]]
+      });
+    }
   }
-
-}
+  
