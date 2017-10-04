6d41ccaa892753b4e9462c658cf130596399b127
Simdi <ezeanietochisimdi@gmail.com>
2017-10-04 20:19:42 +0100

    Updated

--- src/app/system-modules/beneficiary/list-beneficiary/list-beneficiary.component.ts
+++ src/app/system-modules/beneficiary/list-beneficiary/list-beneficiary.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+import { Router, NavigationEnd } from '@angular/router';
+import { FormControl } from '@angular/forms';
+import { UserTypeService } from './../../../services/common/user-type.service';
+import { HeaderEventEmitterService } from '../../../services/event-emitters/header-event-emitter.service';
+// import * from '../../../rxjs/rxjs.extentions';
+import 'rxjs/add/operator/filter';
 
 @Component({
   selector: 'app-list-beneficiary',
@@ -6,10 +12,33 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./list-beneficiary.component.scss']
 })
 export class ListBeneficiaryComponent implements OnInit {
+  listsearchControl = new FormControl();
+  filterTypeControl = new FormControl('All');
+  createdByControl = new FormControl();
+  utilizedByControl = new FormControl();
+  statusControl = new FormControl('All');
+  previousUrl: string = '';
+  beneficiaries: any = <any>[];
 
-  constructor() { }
+  constructor(
+    private _router: Router,
+    private _headerEventEmitter: HeaderEventEmitterService,
+    private _userTypeService: UserTypeService
+  ) {
+    this._router.events
+      .filter(event => event instanceof NavigationEnd)
+      .subscribe(e => {
+      });
+  }
 
   ngOnInit() {
+    this._headerEventEmitter.setRouteUrl('Beneficiary List');
+    this._headerEventEmitter.setMinorRouteUrl('All Beneficiaries');
+
+    this._userTypeService.findAll().then(payload => {
+        console.log(payload)
+      })
   }
+
 
 }
