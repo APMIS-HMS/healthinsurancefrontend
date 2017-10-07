595d6010f6b251fc87a5bae047ceb8fdf7d8742b
aiyk <aiyk.ekwe@gmail.com>
2017-10-07 15:02:27 +0100

    provider employer

--- src/app/system-modules/employer/employer.module.ts
+++ src/app/system-modules/employer/employer.module.ts
@@ -9,13 +9,14 @@ import { employerRoutes } from './employer.route';
 import { EmployerComponent } from './employer.component';
 import { EmployerDetailsComponent } from './employer-details/employer-details.component';
 import { NewEmployerComponent } from './new-employer/new-employer.component';
+import { EmployerTopBarComponent } from './employer-details/employer-top-bar/employer-top-bar.component';
 
 @NgModule({
     imports: [
         SharedModule,
         employerRoutes
     ],
-    declarations: [EmployerComponent, EmployerDetailsComponent, NewEmployerComponent],
+    declarations: [EmployerComponent, EmployerDetailsComponent, NewEmployerComponent, EmployerTopBarComponent],
     providers: [FacilityService, IndustryService, CountryService, BankService, ContactPositionService,
         UserTypeService, SystemModuleService]
 })
