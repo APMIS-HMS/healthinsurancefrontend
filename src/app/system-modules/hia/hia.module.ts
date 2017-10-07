b9dd68e1353eaa143d8a37756c0d648c8de796f9
Simdi <ezeanietochisimdi@gmail.com>
2017-10-07 12:59:01 +0100

    Updated

--- src/app/system-modules/hia/hia.module.ts
+++ src/app/system-modules/hia/hia.module.ts
@@ -4,7 +4,6 @@ import { SharedModule } from '../../shared-modules/shared.module';
 import { hiaRoutes } from './hia.route';
 import { HiaComponent } from './hia.component';
 import { NewHiaComponent } from './new-hia/new-hia.component';
-import { ListHiaComponent } from './list-hia/list-hia.component';
 import { HiaDetailsComponent } from './hia-details/hia-details.component';
 import { TabTopBarComponent } from './hia-details/tab-top-bar/tab-top-bar.component';
 
@@ -13,7 +12,7 @@ import { TabTopBarComponent } from './hia-details/tab-top-bar/tab-top-bar.compon
         SharedModule,
         hiaRoutes
     ],
-    declarations: [HiaComponent, NewHiaComponent, ListHiaComponent, HiaDetailsComponent, TabTopBarComponent],
+    declarations: [HiaComponent, NewHiaComponent, HiaDetailsComponent, TabTopBarComponent],
     providers: []
 })
 export class HiaModule { }
