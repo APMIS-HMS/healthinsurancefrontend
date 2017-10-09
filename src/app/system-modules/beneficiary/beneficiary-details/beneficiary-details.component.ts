9a7c1fbca7230f8f64b162fc80cbe19100beb02d
aiyk <aiyk.ekwe@gmail.com>
2017-10-09 09:13:54 +0100

    beneficiary in progress

--- src/app/system-modules/beneficiary/beneficiary-details/beneficiary-details.component.ts
+++ src/app/system-modules/beneficiary/beneficiary-details/beneficiary-details.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormControl } from '@angular/forms';
 
 @Component({
   selector: 'app-beneficiary-details',
@@ -7,9 +8,121 @@ import { Component, OnInit } from '@angular/core';
 })
 export class BeneficiaryDetailsComponent implements OnInit {
 
-  constructor() { }
+  listsearchControl = new FormControl();
+	premiumsearchControl = new FormControl();
 
-  ngOnInit() {
+	tab_details = true;
+	tab_preauthorization = false;
+	tab_plans = false;
+	tab_beneficiaries = false;
+	tab_employers = false;
+  tab_payment = false;
+  tab_claims = false;
+	tab_complaints = false;
+	tab_referals = false; 
+
+	constructor() { }
+
+  ngOnInit() { }
+  
+  tabDetails_click(){
+    this.tab_details = true;
+    this.tab_preauthorization = false;
+    this.tab_plans = false;
+    this.tab_beneficiaries = false;
+    this.tab_employers = false;
+    this.tab_payment = false;
+    this.tab_claims = false;
+    this.tab_complaints = false;
+    this.tab_referals = false;
+  }
+  tabPreauthorization_click(){
+    this.tab_details = false;
+    this.tab_preauthorization = true;
+    this.tab_plans = false;
+    this.tab_beneficiaries = false;
+    this.tab_employers = false;
+    this.tab_payment = false;
+    this.tab_claims = false;
+    this.tab_complaints = false;
+    this.tab_referals = false;
+  }
+  tabPlans_click(){
+    this.tab_details = false;
+    this.tab_preauthorization = false;
+    this.tab_plans = true;
+    this.tab_beneficiaries = false;
+    this.tab_employers = false;
+    this.tab_payment = false;
+    this.tab_claims = false;
+    this.tab_complaints = false;
+    this.tab_referals = false;
+  }
+  tabBeneficiaries_click(){
+    this.tab_details = false;
+    this.tab_preauthorization = false;
+    this.tab_plans = false;
+    this.tab_beneficiaries = true;
+    this.tab_employers = false;
+    this.tab_payment = false;
+    this.tab_claims = false;
+    this.tab_complaints = false;
+    this.tab_referals = false;
+  }
+  tabEmployers_click(){
+    this.tab_details = false;
+    this.tab_preauthorization = false;
+    this.tab_plans = false;
+    this.tab_beneficiaries = false;
+    this.tab_employers = true;
+    this.tab_payment = false;
+    this.tab_claims = false;
+    this.tab_complaints = false;
+    this.tab_referals = false;
+  }
+  tabPayment_click(){
+    this.tab_details = false;
+    this.tab_preauthorization = false;
+    this.tab_plans = false;
+    this.tab_beneficiaries = false;
+    this.tab_employers = false;
+    this.tab_payment = true;
+    this.tab_claims = false;
+    this.tab_complaints = false;
+    this.tab_referals = false;
+  }
+  tabClaims_click(){
+    this.tab_details = false;
+    this.tab_preauthorization = false;
+    this.tab_plans = false;
+    this.tab_beneficiaries = false;
+    this.tab_employers = false;
+    this.tab_payment = false;
+    this.tab_claims = true;
+    this.tab_complaints = false;
+    this.tab_referals = false;
+  }
+  tabComplaints_click(){
+    this.tab_details = false;
+    this.tab_preauthorization = false;
+    this.tab_plans = false;
+    this.tab_beneficiaries = false;
+    this.tab_employers = false;
+    this.tab_payment = false;
+    this.tab_claims = false;
+    this.tab_complaints = true;
+    this.tab_referals = false;
+  }
+  tabReferals_click(){
+    this.tab_details = false;
+    this.tab_preauthorization = false;
+    this.tab_plans = false;
+    this.tab_beneficiaries = false;
+    this.tab_employers = false;
+    this.tab_payment = false;
+    this.tab_claims = false;
+    this.tab_complaints = false;
+    this.tab_referals = true;
   }
 
 }
