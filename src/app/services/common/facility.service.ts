abc63d69e2964ac2ee1523016122e321079d1d98
sabaoth <sunday@apmis.ng>
2017-10-20 10:48:39 +0100

    updates

--- src/app/services/common/facility.service.ts
+++ src/app/services/common/facility.service.ts
@@ -1,3 +1,4 @@
+import { Router } from '@angular/router';
 import { SystemModuleService } from './system-module.service';
 import { SocketService, RestService } from './../../feathers/feathers.service';
 import { Injectable } from '@angular/core';
@@ -18,7 +19,7 @@ export class FacilityService {
     private _socketService: SocketService,
     private _restService: RestService,
     private locker: CoolLocalStorage,
-    private _systmeService: SystemModuleService
+    private _systmeService: SystemModuleService,
   ) {
     this._rest = _restService.getService('facilities');
     this._socket = _socketService.getService('facilities');
@@ -27,7 +28,12 @@ export class FacilityService {
   find(query: any) {
     return new Promise((resolve, reject) => {
       resolve(this._socketService.authenticateUser('facilities').then((socket: any) => {
-        return socket.find(query);
+        if(socket != undefined){
+          return socket.find(query);
+        }else{
+          console.log(socket)
+          reject('a promise transaction has failed');
+        }
       }))
     });
   }
