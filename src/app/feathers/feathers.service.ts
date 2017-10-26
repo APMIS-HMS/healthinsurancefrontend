6cc06888447421004720f1e4970c4ddc91558432
Simdi <ezeanietochisimdi@gmail.com>
2017-10-26 11:33:31 +0100

    Updated

--- src/app/feathers/feathers.service.ts
+++ src/app/feathers/feathers.service.ts
@@ -11,16 +11,16 @@ import { Injectable } from '@angular/core';
 const rx = require('feathers-reactive');
 const RxJS = require('rxjs');
 
-//const HOST = 'http://172.16.16.29:3031'; // Live
+// const HOST = 'http://172.16.16.29:3031'; // Live
 // const HOST = 'http://192.168.10.8:3031'; // Live
-//const HOST = 'http://localhost:3031'; // Your base server URL here
-const HOST = 'http://insuranceapi.azurewebsites.net';
+const HOST = 'http://localhost:3031'; // Your base server URL here
+// const HOST = 'http://insuranceapi.azurewebsites.net';
 
 @Injectable()
 export class SocketService {
     public socket: any;
     public _app: any;
-    public onlineStatus = false; 
+    public onlineStatus = false;
 
     constructor(
         private locker: CoolLocalStorage, private _systemService: SystemModuleService
