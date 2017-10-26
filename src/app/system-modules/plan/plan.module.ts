ac60dac208312ded42dec50025d32668f6a8575d
aiyk <aiyk.ekwe@gmail.com>
2017-10-26 22:51:07 +0100

    upsates

--- src/app/system-modules/plan/plan.module.ts
+++ src/app/system-modules/plan/plan.module.ts
@@ -11,10 +11,6 @@ import { planRoutes } from './plan.route';
 import { PlanComponent } from './plan.component';
 import { UserTypeService } from '../../services/common/user-type.service';
 import { LashmaCodesListComponent } from './details-plan/lashma-codes-list/lashma-codes-list.component';
-import { EditDrugCodeComponent } from './details-plan/lashma-codes-list/edit-drug-code/edit-drug-code.component';
-import { EditInvestigationCodeComponent } from './details-plan/lashma-codes-list/edit-investigation-code/edit-investigation-code.component';
-import { EditProcedureCodeComponent } from './details-plan/lashma-codes-list/edit-procedure-code/edit-procedure-code.component';
-import { EditVisitCodeComponent } from './details-plan/lashma-codes-list/edit-visit-code/edit-visit-code.component';
 
 @NgModule({
   imports: [
@@ -23,7 +19,7 @@ import { EditVisitCodeComponent } from './details-plan/lashma-codes-list/edit-vi
     ReactiveFormsModule,
     planRoutes
 ],
-  declarations: [PlanComponent, DetailsPlanComponent, ListPlansComponent, NewPlanComponent, LashmaCodesListComponent, EditDrugCodeComponent, EditInvestigationCodeComponent, EditProcedureCodeComponent, EditVisitCodeComponent],
+  declarations: [PlanComponent, DetailsPlanComponent, ListPlansComponent, NewPlanComponent, LashmaCodesListComponent],
   providers: [PlanService, PlanTypeService]
 })
-export class PlanModule { }
+export class PlanModule { } 
