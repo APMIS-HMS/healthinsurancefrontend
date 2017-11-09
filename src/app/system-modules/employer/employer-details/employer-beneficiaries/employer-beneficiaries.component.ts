337f2fe4dec265328358f2ab0414af764b56a0ac
starday01 <sunday@apmis.ng>
2017-11-09 08:13:18 +0100

    updates

--- src/app/system-modules/employer/employer-details/employer-beneficiaries/employer-beneficiaries.component.ts
+++ src/app/system-modules/employer/employer-details/employer-beneficiaries/employer-beneficiaries.component.ts
@@ -1,7 +1,7 @@
 
 import { Component, OnInit } from '@angular/core';
 import { FormControl } from '@angular/forms';
-import { Router } from '@angular/router';
+import { Router, ActivatedRoute } from '@angular/router';
 import { LoadingBarService } from '@ngx-loading-bar/core';
 import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 import { CoolLocalStorage } from 'angular2-cool-storage';
@@ -16,6 +16,7 @@ import { HeaderEventEmitterService } from '../../../../services/event-emitters/h
   styleUrls: ['./employer-beneficiaries.component.scss']
 })
 export class EmployerBeneficiariesComponent implements OnInit {
+  mainBeneficiaries: any;
   user: any;
   currentPlatform: any;
   beneficiaries: any = <any>[];
@@ -45,7 +46,8 @@ export class EmployerBeneficiariesComponent implements OnInit {
     private _systemService: SystemModuleService,
     private _facilityService: FacilityService,
     private _locker: CoolLocalStorage,
-    private _policyService: PolicyService
+    private _policyService: PolicyService,
+    private _route: ActivatedRoute
   ) { }
 
   ngOnInit() {
@@ -53,10 +55,118 @@ export class EmployerBeneficiariesComponent implements OnInit {
     this._headerEventEmitter.setMinorRouteUrl('All pending payments');
     this.user = (<any>this._locker.getObject('auth')).user;
     this._getCurrentPlatform();
+
+    this._route.params.subscribe(param => {
+      console.log(param)
+      if (param.id !== undefined) {
+        this._getFacility(param.id);
+      }
+    })
+  }
+  private _getFacility(id) {
+    this._facilityService.get(id, {}).then(payload => {
+      console.log(payload)
+      this.applyQuery(payload);
+    }).catch(error => {
+      console.log(error);
+    })
+  }
+  private _getAllPolicies(query) {
+    console.log(query)
+    try {
+      this._systemService.on();
+      this._policyService.find(query).then((res: any) => {
+        this.loading = false;
+        console.log(res)
+        if (res.data.length > 0) {
+          res.data.forEach(policy => {
+            let principal = policy.principalBeneficiary;
+            principal.isPrincipal = true;
+            principal.hia = policy.hiaId;
+            principal.policyId = policy._id;
+            principal.isActive = policy.isActive;
+            principal.dependantCount = policy.dependantBeneficiaries.length;
+            principal.planTypeId = policy.planTypeId;
+            principal.policy = policy;
+            this.beneficiaries.push(principal);
+            policy.dependantBeneficiaries.forEach(innerPolicy => {
+              innerPolicy.beneficiary.person = innerPolicy.beneficiary.personId;
+              innerPolicy.beneficiary.isPrincipal = false;
+              innerPolicy.beneficiary.principalId = principal._id;
+              innerPolicy.beneficiary.policyId = policy._id;
+              innerPolicy.beneficiary.policy = policy;
+              innerPolicy.beneficiary.hia = policy.hiaId;
+              innerPolicy.beneficiary.isActive = policy.isActive;
+              innerPolicy.beneficiary.planTypeId = policy.planTypeId;
+              this.beneficiaries.push(innerPolicy.beneficiary);
+            });
+          });
+        }
+        this.mainBeneficiaries = this.beneficiaries;
+        this._systemService.off();
+        this.loading = false;
+        console.log(this.mainBeneficiaries)
+      }).catch(err => {
+        console.log(err)
+        this.loading = false;
+        this._systemService.off();
+        this._toastr.error('Error has occured please contact your administrator!', 'Error!');
+      });
+    } catch (error) {
+      console.log(error)
+      this.loading = false;
+      this._toastr.error('Error has occured please contact your administrator!', 'Error!');
+    }
+  }
+
+  private applyQuery(facility) {
+    console.log(facility)
+    if (facility !== undefined) {
+      console.log(1)
+      // { platformOwnerNumber: { $regex: value, '$options': 'i' } },
+      if (facility.facilityType !== undefined && facility.facilityType.name === 'Provider') {
+        console.log(2)
+        this._getAllPolicies({
+          query: {
+            'sponsor._id': facility._id,
+            $limit: 200,
+            $sort: { createdAt: -1 },
+            $select: { 'hiaId.name': 1, 'principalBeneficiary': 1, 'dependantBeneficiaries': 1, 'isActive': 1, 'providerId': 1 }
+          }
+        });
+      } else if (facility.facilityType !== undefined && facility.facilityType.name === 'Employer') {
+        console.log('employer')
+        this._getAllPolicies({
+          query: {
+            'sponsor._id': facility._id,
+            $limit: 200,
+            $sort: { createdAt: -1 },
+            $select: { 'hiaId.name': 1, 'principalBeneficiary': 1, 'dependantBeneficiaries': 1, 'isActive': 1 }
+          }
+        });
+      } else if (!!this.user.userType && this.user.userType.name === 'Platform Owner') {
+        this._getAllPolicies({
+          query: {
+            'platformOwnerId._id': this.user.facilityId._id,
+            $limit: 200,
+            $sort: { createdAt: -1 },
+            $select: { 'hiaId.name': 1, 'principalBeneficiary': 1, 'dependantBeneficiaries': 1, 'isActive': 1 }
+          }
+        });
+      } else {
+        this._getAllPolicies({
+          query: {
+            'platformOwnerId._id': this.currentPlatform._id,
+            $limit: 200,
+            $sort: { createdAt: -1 },
+            $select: { 'platformOwnerId.$': 1, 'hiaId.name': 1, 'principalBeneficiary': 1, 'dependantBeneficiaries': 1, 'isActive': 1 }
+          }
+        });
+      }
+    }
   }
 
   private _getPolicies() {
-    console.log(this.currentPlatform);
     this._systemService.on();
     this._policyService.find({
       query: {
@@ -65,7 +175,6 @@ export class EmployerBeneficiariesComponent implements OnInit {
         $sort: { createdAt: -1 }
       }
     }).then((res: any) => {
-      console.log(res);
       this.loading = false;
       res.data.forEach(policy => {
         policy.dueDate = this.addDays(new Date(), policy.premiumPackageId.durationInDay);
@@ -98,10 +207,10 @@ export class EmployerBeneficiariesComponent implements OnInit {
     });
   }
 
-  editRow(){
+  editRow() {
     this.isEdit = !this.isEdit;
   }
-  navigateDetailBeneficiary(){
+  navigateDetailBeneficiary() {
 
   }
 }
