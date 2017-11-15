e2de2acf76489ceefadbf556f74690335de0c0a9
starday01 <sunday@apmis.ng>
2017-11-15 13:06:06 +0100

    updates

--- src/app/system-modules/employer/employer.module.ts
+++ src/app/system-modules/employer/employer.module.ts
@@ -27,20 +27,15 @@ import { BatchedComponent } from './employer-details/premium-payment-tab/batched
         MyDatePickerModule,
         Angular4PaystackModule
     ],
-<<<<<<< HEAD
-    declarations: [EmployerComponent, EmployerDetailsComponent, NewEmployerComponent, EmployerBeneficiariesComponent,ExcelUploadItemsComponent],
-    providers: [FacilityService, IndustryService, CountryService, BankService, ContactPositionService, PlanService,
-        UserTypeService, SystemModuleService, BeneficiaryService, PolicyService, TitleService, ClaimService,MaritalStatusService,PlanTypeService]
-=======
     declarations: [
         EmployerComponent,
         EmployerDetailsComponent,
         NewEmployerComponent, EmployerBeneficiariesComponent,
         PremiumPaymentTabComponent,
         UnbatchedComponent,
+        ExcelUploadItemsComponent,
         BatchedComponent],
     providers: [FacilityService, IndustryService, CountryService, BankService, ContactPositionService, PlanService,
         UserTypeService, SystemModuleService, BeneficiaryService, PolicyService, TitleService, ClaimService, PremiumPaymentService]
->>>>>>> e6918bfd09bfb73a3a798dee7345835950a1ece0
 })
 export class EmployerModule { }
