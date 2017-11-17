98f6cd66233a462ab52f93102e7f17b5cf32eeff
karosi12 <adekayor@gmail.com>
2017-11-17 15:47:09 +0100

    change password

--- src/app/system-modules/top-bar/change-pass/change-pass.component.ts
+++ src/app/system-modules/top-bar/change-pass/change-pass.component.ts
@@ -1,61 +1,127 @@
-import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, FormBuilder, Validators } from '@angular/forms';
-import { PasswordValidation} from '../../../services/common/password-validation';
-import { AuthService } from '../../../auth/services/auth.service';
-import { CoolLocalStorage } from 'angular2-cool-storage';
-import { User } from '../../../models/setup/user';
+import { Component, OnInit } from "@angular/core";
+import {
+  FormGroup,
+  FormControl,
+  FormBuilder,
+  Validators
+} from "@angular/forms";
+import { PasswordValidation } from "../../../services/common/password-validation";
+import { AuthService } from "../../../auth/services/auth.service";
+import { CoolLocalStorage } from "angular2-cool-storage";
+import { User } from "../../../models/setup/user";
+import { Injectable } from "@angular/core";
+import { ToastsManager } from "ng2-toastr/ng2-toastr";
+
+const host = require('../../../feathers/feathers.service'); // "http://localhost:3031"; 
+const feathers = require("feathers/client");
+const socketio = require("feathers-socketio/client");
+const io = require("socket.io-client");
+const hooks = require("feathers-hooks");
+const rest = require("feathers-rest/client");
+const rx = require("feathers-reactive");
+const RxJS = require("rxjs");
+const authentication = require("feathers-authentication-client");
+
+@Injectable()
+export class ChangePasswordSocketService {
+  public socket: any;
+  public _app: any;
+
+  constructor() {
+    this.socket = io(host.HOST);
+    this._app = feathers()
+      .configure(socketio(this.socket))
+      .configure(rx(RxJS, { listStrategy: "always" }))
+      .configure(hooks())
+      .configure(authentication({ storage: window.sessionStorage }));
+  }
+  chLoginIntoApp(query: any) {
+    return this._app.authenticate({
+      strategy: "local",
+      email: query.email,
+      password: query.password
+    });
+  }
+}
+
+// password RvnYUv4i
+
+@Injectable()
+export class ChangePasswordAuthService {
+  _chSocket;
+  constructor(private _chSocketService: ChangePasswordSocketService) {
+  }
+
+  chLogin(query: any) {
+    return this._chSocketService.chLoginIntoApp(query);
+  }
+}
 
 @Component({
-  selector: 'app-change-pass',
-  templateUrl: './change-pass.component.html',
-  styleUrls: ['./change-pass.component.scss']
+  selector: "app-change-pass",
+  templateUrl: "./change-pass.component.html",
+  styleUrls: ["./change-pass.component.scss"]
 })
 export class ChangePassComponent implements OnInit {
-
   changePassFormGroup: FormGroup;
-  user: User= <User>{};
-
-  constructor(private _fb: FormBuilder,
-     private _authService: AuthService,
-     private _locker: CoolLocalStorage) { }
+  user: User = <User>{};
+  _app: any;
+  constructor(
+    private _fb: FormBuilder,
+    private _chAuthService: ChangePasswordAuthService,
+    private _locker: CoolLocalStorage,
+    private _authService: AuthService,
+    private _toastr: ToastsManager,
+  ) {}
 
   ngOnInit() {
-
     this.user = <any>this._locker.getItem('auth');
-    console.log(this.user);
-    this.changePassFormGroup = this._fb.group({
-      email: ['', [<any>Validators.required]],
-      password: ['', [<any>Validators.required]],
-    })
-    //   cpassword: ['', [<any>Validators.required]]
-		// }, {
-    //   validator: PasswordValidation.MatchPassword
-    // });
+    this.changePassFormGroup = this._fb.group(
+      {
+        old_pass: ["", [<any>Validators.required]],
+        password: ["", [<any>Validators.required]],
+        cpassword: ["", [<any>Validators.required]]
+      },
+      {
+        validator: PasswordValidation.MatchPassword
+      }
+    );
   }
 
   changePassword(value: any, valid: boolean) {
-    // console.log(value);
-    // this.user = <any>this._locker.getItem('auth');
-    //  const getUserPassword = value.old_pass;
-    
-    // console.log(value);
+    this.user = JSON.parse(<any>this._locker.getItem("auth")).user;
+    const getUserPassword = value.old_pass,
+      userId = this.user._id;
 
-    // console.log({email: this.user.email, password: getUserPassword});
-
-    // const toCheck = {email: this.user.email, password: getUserPassword};
-    this._authService.login(value)
-      .then((result) => {
-        console.log(result);
+    const toCheck = { email: this.user.email, password: getUserPassword };
+    return this._chAuthService
+      .chLogin(toCheck)
+      .then(logindetails => {
+        return logindetails;
       })
-      .catch((err) => {
-        console.log('Wrong user', err);
+      .then(result => {  // RvnYUv4i
+        const newpwd = {password: value.cpassword};
+        this._authService
+          .patch(userId, newpwd, {})
+          .then(updatedPwd => {
+            this._toastr.success(
+              'Password was successfully changed',
+              'Success!'
+            );
+        return this._authService.logOut();
+          })
+          .catch(err => {
+            this._toastr.error(
+              'Unable to change password',
+              'Error!'
+            );
+          });
+      })
+      .catch(err => {
+        this._toastr.error(
+          'Wrong Password',
+          'Error!'
+        );
       });
-
-    // this._authService.changePassword({password: value.password})
-    //   .then((resetPwd) => {
-    //     console.log(resetPwd);
-    //   });
-  // }
-
-}
+  }
 }
