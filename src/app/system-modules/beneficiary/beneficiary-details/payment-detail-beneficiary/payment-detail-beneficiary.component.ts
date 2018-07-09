6f5fec8e77f0aec11fa8bb34af8f54eabd5ec7d6
Chisimdi Damian Ezeanieto <chisimdi.ezeanieto@apmis.ng>
2018-07-09 17:27:16 +0100

    Done with payment details for beneficiary.

--- src/app/system-modules/beneficiary/beneficiary-details/payment-detail-beneficiary/payment-detail-beneficiary.component.ts
+++ src/app/system-modules/beneficiary/beneficiary-details/payment-detail-beneficiary/payment-detail-beneficiary.component.ts
@@ -1,35 +1,35 @@
-import { Component, ElementRef, OnInit } from "@angular/core";
+import { Component, ElementRef, OnInit } from '@angular/core';
 import {
   FormBuilder,
   FormControl,
   FormGroup,
   Validators
-} from "@angular/forms";
-import { ActivatedRoute, Router } from "@angular/router";
-import { LoadingBarService } from "@ngx-loading-bar/core";
-import { CoolLocalStorage } from "angular2-cool-storage";
-import { ToastsManager } from "ng2-toastr/ng2-toastr";
+} from '@angular/forms';
+import { ActivatedRoute, Router } from '@angular/router';
+import { LoadingBarService } from '@ngx-loading-bar/core';
+import { CoolLocalStorage } from 'angular2-cool-storage';
+import { ToastsManager } from 'ng2-toastr/ng2-toastr';
 
-import { environment } from "../../../../../environments/environment";
-import { HeaderEventEmitterService } from "../../../../services/event-emitters/header-event-emitter.service";
+import { environment } from '../../../../../environments/environment';
+import { HeaderEventEmitterService } from '../../../../services/event-emitters/header-event-emitter.service';
 // import { Angular4PaystackComponent } from 'angular4-paystack';
 import {
   FLUTTERWAVE_PUBLIC_KEY,
   PAYMENTTYPES,
   PAYSTACK_CLIENT_KEY
-} from "../../../../services/globals/config";
+} from '../../../../services/globals/config';
 import {
   BeneficiaryService,
   FacilityService,
   PolicyService,
   PremiumPaymentService,
   SystemModuleService
-} from "../../../../services/index";
+} from '../../../../services/index';
 
 @Component({
-  selector: "app-payment-detail-beneficiary",
-  templateUrl: "./payment-detail-beneficiary.component.html",
-  styleUrls: ["./payment-detail-beneficiary.component.scss"]
+  selector: 'app-payment-detail-beneficiary',
+  templateUrl: './payment-detail-beneficiary.component.html',
+  styleUrls: ['./payment-detail-beneficiary.component.scss']
 })
 export class PaymentDetailBeneficiaryComponent implements OnInit {
   paymentOptionGroup: FormGroup;
@@ -42,17 +42,18 @@ export class PaymentDetailBeneficiaryComponent implements OnInit {
   paystackClientKey: string = PAYSTACK_CLIENT_KEY;
   flutterwaveClientKey: string = FLUTTERWAVE_PUBLIC_KEY;
   withPaystack: boolean = true;
+  withFlutterwave: boolean = false;
   cashPayment: boolean = false;
   chequePayment: boolean = false;
   refKey: number;
-  paymentType: string = "e-Payment";
+  paymentType: string = 'Paystack';
   previousPolicyLoading: boolean = true;
   showPayment: boolean = false;
   isForRenewal: boolean = false;
   openCashPaymentModal: boolean = false;
   cashPaymentProcessing: boolean = false;
   paymentTypes: any = PAYMENTTYPES;
-  payment: string = "flutterwave"; // This is either flutterwave or paystack
+  payment: string = 'flutterwave'; // This is either flutterwave or paystack
 
   platformName: string;
   constructor(
@@ -81,11 +82,9 @@ export class PaymentDetailBeneficiaryComponent implements OnInit {
   // }
 
   ngOnInit() {
-    this._headerEventEmitter.setRouteUrl("Beneficiary Payment");
-    this._headerEventEmitter.setMinorRouteUrl(
-      "Payment and payment history for beneficiary"
-    );
-    this.user = (<any>this._locker.getObject("auth")).user;
+    this._headerEventEmitter.setRouteUrl('Beneficiary Payment');
+    this._headerEventEmitter.setMinorRouteUrl('Payment and payment history for beneficiary');
+    this.user = (<any>this._locker.getObject('auth')).user;
 
     this._route.parent.params.subscribe(param => {
       if (!!param.id) {
@@ -96,24 +95,29 @@ export class PaymentDetailBeneficiaryComponent implements OnInit {
     });
 
     this.paymentOptionGroup = this._fb.group({
-      paymentOption: ["e-Payment", [<any>Validators.required]]
+      paymentOption: ['Paystack', [<any>Validators.required]]
     });
 
     this.cashPaymentGroup = this._fb.group({
       amount: [{ value: 0, disabled: true }, [<any>Validators.required]],
-      comment: [""]
+      comment: ['']
     });
 
-    this.paymentOptionGroup.controls["paymentOption"].valueChanges.subscribe(
-      value => {
-        console.log(value);
+    this.paymentOptionGroup.controls['paymentOption'].valueChanges.subscribe(value => {
         this.paymentType = value;
-        if (value === "Cash" || value === "Cheque") {
+        if (value === 'Cash' || value === 'Cheque') {
           this.cashPayment = true;
           this.chequePayment = true;
           this.withPaystack = false;
-        } else {
+          this.withFlutterwave = false;
+        } else if (value === 'Paystack') {
           this.withPaystack = true;
+          this.withFlutterwave = false;
+          this.cashPayment = false;
+          this.chequePayment = false;
+        } else if (value === 'Flutterwave') {
+          this.withFlutterwave = true;
+          this.withPaystack = false;
           this.cashPayment = false;
           this.chequePayment = false;
         }
@@ -121,14 +125,11 @@ export class PaymentDetailBeneficiaryComponent implements OnInit {
     );
 
     this.refKey =
-      (this.user ? this.user._id.substr(20) : "") + new Date().getTime();
+      (this.user ? this.user._id.substr(20) : '') + new Date().getTime();
   }
 
   private _getPolicyDetails(routeId) {
-    this._policyService
-      .get(routeId, {})
-      .then((res: any) => {
-        console.log(res);
+    this._policyService.get(routeId, {}).then((res: any) => {
         if (res._id) {
           res.premiumPackageId.amountInKobo = res.premiumPackageId.amount * 100;
           res.dueDate = this.addDays(
@@ -138,114 +139,99 @@ export class PaymentDetailBeneficiaryComponent implements OnInit {
           this.isForRenewal = res.isPaid;
           this.policy = res;
         }
-      })
-      .catch(err => console.log(err));
+      }).catch(err => console.log(err));
   }
 
   private _getPreviousPolicies(routeId, ownerId: any) {
-    this._policyService
-      .find({
-        query: {
-          "platformOwnerId._id": ownerId,
-          _id: routeId,
-          isPaid: true,
-          $sort: { createdAt: -1 }
-        }
-      })
-      .then((res: any) => {
-        console.log(res);
-        this.previousPolicyLoading = false;
-        if (res.data.length > 0) {
-          console.log(res.data);
-          res.data.forEach(policy => {
-            policy.dueDate = this.addDays(
-              new Date(),
-              policy.premiumPackageId.durationInDay
-            );
-            this.previousPolicies.push(policy);
-          });
-          // this.previousPolicies = res.data;
-        }
-      })
-      .catch(err => console.log(err));
+    this._policyService.find({
+      query: {
+        'platformOwnerId._id': ownerId,
+        _id: routeId,
+        isPaid: true,
+        $sort: { createdAt: -1 }
+      }
+    }).then((res: any) => {
+      this.previousPolicyLoading = false;
+      if (res.data.length > 0 && res.data[0].validityPeriods && res.data[0].validityPeriods.length > 0) {
+        // console.log(res.data);
+        // res.data.forEach(policy => {
+        //   policy.dueDate = this.addDays(new Date(), policy.premiumPackageId.durationInDay);
+        //   this.previousPolicies.push(policy);
+        // });
+        // this.previousPolicies = res.data;
+        this.previousPolicies = res.data[0].validityPeriods;
+      }
+    }).catch(err => console.log(err));
   }
 
   private _getCurrentPlatform() {
-    this._facilityService
-      .find({
-        query: {
-          shortName: this.platformName,
-          $select: ["name", "shortName", "address.state"]
-        }
-      })
-      .then((res: any) => {
-        if (res.data.length > 0) {
-          this.currentPlatform = res.data[0];
-          this._getPreviousPolicies(this.routeId, this.currentPlatform._id);
-        }
-      })
-      .catch(err => {
-        console.log(err);
-        this._router.navigate(["auth/login"]);
-      });
+    this._facilityService.find({
+      query: {
+        shortName: this.platformName,
+        $select: ['name', 'shortName', 'address.state']
+      }
+    }).then((res: any) => {
+      if (res.data.length > 0) {
+        this.currentPlatform = res.data[0];
+        this._getPreviousPolicies(this.routeId, this.currentPlatform._id);
+      }
+    }).catch(err => {
+      console.log(err);
+      this._router.navigate(['auth/login']);
+    });
   }
 
   paymentDone(data) {
     console.log(data);
+    let resData;
     let policies = [];
     // All policies that is being paid for.
     policies.push({
       policyId: this.policy.policyId,
       policyCollectionId: this.policy._id
     });
+    console.log(this.paymentType);
+    if (this.paymentType === 'Flutterwave') {
+      resData = {
+        data: data.data.data,
+        tx: {
+          charged_amount: data.tx.charged_amount,
+          customer: data.tx.customer,
+          flwRef: data.tx.flwRef,
+          txRef: data.tx.txRef,
+          orderRef: data.tx.orderRef,
+          paymentType: data.tx.paymentType,
+          raveRef: data.tx.raveRef,
+          status: data.tx.status
+        }
+      };
+    }
 
-    let flutterwaveRes = {
-      data: data.data.data,
-      tx: {
-        charged_amount: data.tx.charged_amount,
-        customer: data.tx.customer,
-        flwRef: data.tx.flwRef,
-        txRef: data.tx.txRef,
-        orderRef: data.tx.orderRef,
-        paymentType: data.tx.paymentType,
-        raveRef: data.tx.raveRef,
-        status: data.tx.status
-      }
-    };
-
-    console.log(flutterwaveRes);
     let ref = {
       platformOwnerId: this.currentPlatform,
-      reference: this.payment === "flutterwave" ? flutterwaveRes : data,
+      reference: (this.paymentType === 'Flutterwave') ? resData : data,
       policies: policies,
-      sponsor: !!this.policy.sponsor ? this.policy.sponsor : "self",
+      sponsor: !!this.policy.sponsor ? this.policy.sponsor : 'self',
       paidBy: this.user,
       requestedAmount: this.policy.premiumPackageId.amount,
       amountPaid: this.policy.premiumPackageId.amount,
       paymentType: this.paymentType
     };
 
-    if (this.payment === "flutterwave") {
-      if (
-        data.tx.chargeResponseCode === "00" ||
-        data.tx.chargeResponseCode === "0"
-      ) {
+    if (this.paymentType === 'Flutterwave') {
+      if (data.tx.chargeResponseCode === '00' || data.tx.chargeResponseCode === '0') {
         // redirect to a success page
-        console.log("Succeeded");
-        ref.reference = flutterwaveRes.tx;
+        console.log('Succeeded');
+        ref.reference = resData.tx;
         this.createPremium(ref);
-      } else if (
-        data.tx.chargeResponseCode === "02" ||
-        data.tx.chargeResponseCode === "2"
-      ) {
+      } else if (data.tx.chargeResponseCode === '02' || data.tx.chargeResponseCode === '2') {
         // Declined payment
-        this._toastr.error(data.respmsg, "Payment Declined");
+        this._toastr.error(data.respmsg, 'Payment Declined');
       } else {
-        this._toastr.error(data.respmsg, "Payment Declined");
+        this._toastr.error(data.respmsg, 'Payment Declined');
       }
     } else {
       // payment is paystack
-      ref.reference = data;
       this.createPremium(ref);
     }
   }
@@ -269,9 +255,7 @@ export class PaymentDetailBeneficiaryComponent implements OnInit {
         facilityId: this.user.facilityId,
         email: this.user.email,
         isActive: this.user.isActive,
-        platformOwnerId: !!this.user.platformOwnerId
-          ? this.user.platformOwnerId
-          : "",
+        platformOwnerId: !!this.user.platformOwnerId ? this.user.platformOwnerId : '',
         phoneNumber: this.user.phoneNumber
       };
 
@@ -282,16 +266,14 @@ export class PaymentDetailBeneficiaryComponent implements OnInit {
         requestedAmount: this.policy.premiumPackageId.amount,
         amountPaid: this.policy.premiumPackageId.amount,
         paymentType: this.paymentType,
-        sponsor: !!this.policy.sponsor ? this.policy.sponsor : "self",
+        sponsor: !!this.policy.sponsor ? this.policy.sponsor : 'self',
         comment: value.comment,
         isActive: true,
-        action: "create"
+        action: 'create'
       };
 
       console.log(ref);
-      this._premiumPaymentService
-        .payWidthCashWithMiddleWare(ref)
-        .then((res: any) => {
+      this._premiumPaymentService.payWidthCashWithMiddleWare(ref).then((res: any) => {
           console.log(res);
           if (!!res) {
             this.showPayment = false;
@@ -299,75 +281,60 @@ export class PaymentDetailBeneficiaryComponent implements OnInit {
             this.openCashPaymentModal = false;
             this._getPolicyDetails(res.body._id);
             this._getPreviousPolicies(this.routeId, this.currentPlatform._id);
-            this._toastr.success(
-              "Policy has been activated successfully.",
-              "Payment Completed!"
-            );
+            this._toastr.success('Policy has been activated successfully.', 'Payment Completed!');
           }
-        })
-        .catch(err => {
+        }).catch(err => {
           console.log(err);
         });
     } else {
       this._toastr.error(
-        "Please fill in all required fields",
-        "Form ValidationError!"
+        'Please fill in all required fields',
+        'Form ValidationError!'
       );
     }
   }
 
   createPremium(ref) {
     // Save into the Premium Payment Service
-    this._premiumPaymentService
-      .create(ref)
-      .then((res: any) => {
+    this._premiumPaymentService.create(ref).then((res: any) => {
         console.log(res);
 
         let verificationData = {
           reference: res.reference,
           premiumId: res._id,
-          payment: this.payment // flutterwave or paystack
+          payment: this.paymentType // flutterwave or paystack
         };
+
         // Call paystack verification API
-        this._premiumPaymentService
-          .verifyPaymentWithMiddleWare(verificationData)
-          .then((verifyRes: any) => {
+        this._premiumPaymentService.verifyPaymentWithMiddleWare(verificationData).then((verifyRes: any) => {
             console.log(verifyRes);
             if (!!verifyRes) {
-              if (verifyRes.body.status === "success") {
+              if (verifyRes.body.status === 'success') {
                 this.showPayment = false;
                 this.isForRenewal = true;
                 this._getPolicyDetails(verifyRes.body.data._id);
-                this._getPreviousPolicies(
-                  this.routeId,
-                  this.currentPlatform._id
-                );
-                this._toastr.success(
-                  "Policy has been activated successfully.",
-                  "Payment Completed!"
-                );
+                this._getPreviousPolicies(this.routeId, this.currentPlatform._id);
+                this._toastr.success('Policy has been activated successfully.', 'Payment Completed!');
               } else {
                 this.showPayment = false;
                 this.isForRenewal = true;
-                this._toastr.error(verifyRes.body.message, "Payment Error!");
+                this._toastr.error(verifyRes.body.message, 'Payment Error!');
               }
             }
-          })
-          .catch(err => {
+          }).catch(err => {
             console.log(err);
           });
-      })
-      .catch(err => {
+      }).catch(err => {
         console.log(err);
       });
   }
 
   onClickPayCash() {
-    this.cashPaymentGroup.controls["amount"].setValue(
+    this.cashPaymentGroup.controls['amount'].setValue(
       this.policy.premiumPackageId.amount
     );
     this.openCashPaymentModal = true;
-    console.log("Pay Cash");
+    console.log('Pay Cash');
   }
 
   modal_close() {
@@ -389,6 +356,6 @@ export class PaymentDetailBeneficiaryComponent implements OnInit {
   }
 
   paymentCancel() {
-    console.log("Close");
+    console.log('Close');
   }
 }
